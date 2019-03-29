a1dc3fc85ad0c8ef79bbbf2405b7d6e1dacdd8cc
Yan Gao <gaoyan126@googlemail.com>
2019-03-29 20:30:55 +0100

    Add sudoku template files and read them

---
+++ pysudoku/pysudoku.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Open a file
+path = "./templates/template1.txt"
+fo = open(path, "r")
+str = fo.read()
+print ("Sudoku template:\n", str)
+
+# Close opened file
+fo.close()
+
+print ("Sudoku result:\n", str)
