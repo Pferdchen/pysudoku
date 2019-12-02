096bc068b72230317a9c3f18d3e7d748c40e1b05
Yan Gao <gaoyan126@google.com>
2019-12-02 20:24:18 +0100

    fix: bug in function reduce_sets_in_square() refactor: extract code in functions and some constants

--- pysudoku/pysudoku.py
+++ pysudoku/pysudoku.py
@@ -1,94 +1,115 @@
 #!/usr/bin/python3
 
 
-def reduce_sets(suggestion_sets, sudoku_matrix, i, j):
+def read_template(path):
+    fo = open(path, "r")
+    template = fo.read()
+    fo.close()
+    return template
+
+
+def init_sudoku(template):
+    matrix = list()
+    for line in template.split(LINE_FEED):
+        row = list()
+        for c in list(line):
+            if c.isdecimal():
+                row.append(int(c))
+            else:
+                row.append(0)
+        matrix.append(row)
+    return matrix
+
+
+def init_suggestion(matrix):
+    suggestion = list()
+    for i in range(9):
+        row = list()
+        for j in range(9):
+            num = matrix[i][j]
+            if num != 0:
+                row.append(set([num]))
+            else:
+                row.append(FULL_SUGGESTIONS.copy())
+        suggestion.append(row)
+    return suggestion
+
+
+def calculate(sudoku, suggestion):
+    for i in range(9):
+        for j in range(9):
+            num = sudoku[i][j]
+            if num != 0:
+                continue
+            else:
+                possible_values = suggestion[i][j]
+                for k in range(9):
+                    if k != j:
+                        possible_values.discard(sudoku[i][k])  # remove on horizontal
+                    if k != i:
+                        possible_values.discard(sudoku[k][j])  # remove on vertical
+
+                x, y = square_position(i, j)
+                for r in range(x, x + 3):
+                    for s in range(y, y + 3):
+                        if r != i and s != j:
+                            possible_values.discard(sudoku[r][s])  # remove in square
+
+                reduce_sets(sudoku, suggestion, i, j)
+
+
+def reduce_sets(sudoku, suggestion, i, j):
     possible_set = suggestion_sets[i][j]
-    if len(possible_set) == 1 and sudoku_matrix[i][j] == 0:
+    if len(possible_set) == 1 and sudoku[i][j] == 0:
         find = possible_set.pop()
-        sudoku_matrix[i][j] = find
+        sudoku[i][j] = find
         possible_set.add(find)  # restore popped number in set
-        reduce_sets_on_axis(suggestion_sets, sudoku_matrix, i, j)
-        reduce_sets_in_square(suggestion_sets, sudoku_matrix, i, j)
+        reduce_sets_on_axis(sudoku, suggestion, i, j)
+        reduce_sets_in_square(sudoku, suggestion, i, j)
 
 
-def reduce_sets_on_axis(suggestion_sets, sudoku_matrix, i, j):
+def reduce_sets_on_axis(sudoku, suggestion, i, j):
     for k in range(9):
-        if k != j and len(suggestion_sets[i][k]) > 1:
-            suggestion_sets[i][k].discard(sudoku_matrix[i][j])
-            reduce_sets(suggestion_sets, sudoku_matrix, i, k)
-        if k != i and len(suggestion_sets[k][j]) > 1:
-            suggestion_sets[k][j].discard(sudoku_matrix[i][j])
-            reduce_sets(suggestion_sets, sudoku_matrix, k, j)
+        if k != j and len(suggestion[i][k]) > 1:
+            suggestion[i][k].discard(sudoku[i][j])
+            reduce_sets(sudoku, suggestion, i, k)
+        if k != i and len(suggestion[k][j]) > 1:
+            suggestion[k][j].discard(sudoku[i][j])
+            reduce_sets(sudoku, suggestion, k, j)
 
 
-def reduce_sets_in_square(suggestion_sets, sudoku_matrix, i, j):
-    for r in range((i // 3) * 3, (i // 3) * 3 + 3):
-        for s in range((j // 3) * 3, (j // 3) * 3 + 3):
-            if r != i and s != j and len(suggestion_sets[r][s]) > 1:
-                suggestion_sets[r][s].discard(sudoku_matrix[i][j])
-                reduce_sets(suggestion_sets, sudoku_matrix, k, j)
+def reduce_sets_in_square(sudoku, suggestion, i, j):
+    x, y = square_position(i, j)
+    for r in range(x, x + 3):
+        for s in range(y, y + 3):
+            if r != i and s != j and len(suggestion[r][s]) > 1:
+                suggestion[r][s].discard(sudoku[i][j])
+                reduce_sets(sudoku, suggestion, r, s)
 
 
-# Open a file
-path = "../templates/template1.txt"
-fo = open(path, "r")
-sudoku_str = fo.read()
-print("Sudoku template:\n" + sudoku_str)
+def square_position(i, j):
+    return (i // 3) * 3, (j // 3) * 3
 
-# Close opened file after usage
-fo.close()
 
-# init sudoku_matrix
-sudoku_matrix = list()
-for line in sudoku_str.split('\n'):
-    sudoku_row = list()
-    for c in list(line):
-        if c.isdecimal():
-            sudoku_row.append(int(c))
-        else:
-            sudoku_row.append(0)
-    sudoku_matrix.append(sudoku_row)
-
-# init suggestion_sets
-suggestion_sets = list()
+# main begin
+LINE_FEED = '\n'
+SQUARE_OFFSET = 3
 FULL_SUGGESTIONS = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-for i in range(9):
-    suggestion_row = list()
-    for j in range(9):
-        num = sudoku_matrix[i][j]
-        if num != 0:
-            suggestion_row.append(set([num]))
-        else:
-            suggestion_row.append(FULL_SUGGESTIONS.copy())
-    suggestion_sets.append(suggestion_row)
 
-#print(suggestion_sets)
+sudoku_template = read_template("../templates/template1.txt")
+print("Sudoku template:" + LINE_FEED + sudoku_template)
 
-# recursive reduce
-for i in range(9):
-    for j in range(9):
-        num = sudoku_matrix[i][j]
-        if num != 0:
-            continue
-        else:
-            possible_values = suggestion_sets[i][j]
-            for k in range(9):
-                if k != j:
-                    possible_values.discard(sudoku_matrix[i][k]) # remove on horizontal
-                if k != i:
-                    possible_values.discard(sudoku_matrix[k][j]) # remove on vertical
+sudoku_matrix = init_sudoku(sudoku_template)
+suggestion_sets = init_suggestion(sudoku_matrix)
+# print(suggestion_sets)
 
-            for r in range((i // 3) * 3, (i // 3) * 3 + 3):
-                for s in range((j // 3) * 3, (j // 3) * 3 + 3):
-                    if r != i and s != j:
-                        possible_values.discard(sudoku_matrix[r][s]) # remove in square
+calculate(sudoku_matrix, suggestion_sets)
+# print(suggestion_sets)
 
-            reduce_sets(suggestion_sets, sudoku_matrix, i, j)
-
-result_str = "Sudoku result:\n"
-for row in sudoku_matrix:
-    for num in row:
-        result_str += '{}'
-        result_str = result_str.format(num)
-    result_str += '\n'
-print(result_str)
+sudoku_result = ""
+for num_line in sudoku_matrix:
+    for cell_num in num_line:
+        sudoku_result += '{}'
+        sudoku_result = sudoku_result.format(cell_num)
+    sudoku_result += LINE_FEED
+print("Sudoku result:" + LINE_FEED + sudoku_result)
